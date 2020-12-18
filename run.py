fd2aab7e5145a4e71d1a84778b64d381a0f70793
JMSCHOOL4038 <76211971+JMSCHOOL4038@users.noreply.github.com>
2020-12-18 22:24:28 +0900

    Update run.py

--- run.py
+++ run.py
@@ -25,9 +25,105 @@ async def on_message(message):
         await message.delete()
     await bot.process_commands(message)
 
+@client.event
+async def on_message(message):
+    message_content = message.content
+    bad = message_content.find("새끼")
+    print(bad)
+    if bad >= 0:
+        await message.channel.send("바른말 고운말을 사용합시다.") 
+        await message.delete()
+    await bot.process_commands(message)
 
+@client.event
+async def on_message(message):
+    message_content = message.content
+    bad = message_content.find("나쁜새끼")
+    print(bad)
+    if bad >= 0:
+        await message.channel.send("바른말 고운말을 사용합시다.") 
+        await message.delete()
+    await bot.process_commands(message)
+
+@client.event
+async def on_message(message):
+    message_content = message.content
+    bad = message_content.find("썅")
+    print(bad)
+    if bad >= 0:
+        await message.channel.send("바른말 고운말을 사용합시다.") 
+        await message.delete()
+     await bot.process_commands(message)
+
+client.event
+async def on_message(message):
+    message_content = message.content
+    bad = message_content.find("년")
+    print(bad)
+    if bad >= 0:
+        await message.channel.send("바른말 고운말을 사용합시다.") 
+        await message.delete()
+    await bot.process_commands(message)
+
+@client.event
+async def on_message(message):
+    message_content = message.content
+    bad = message_content.find("지랄")
+    print(bad)
+    if bad >= 0:
+        await message.channel.send("바른말 고운말을 사용합시다.") 
+        await message.delete()
+    await bot.process_commands(message)
+
+@client.event
+async def on_message(message):
+    message_content = message.content
+    bad = message_content.find("ㅈㄹ")
+    print(bad)
+    if bad >= 0:
+        await message.channel.send("바른말 고운말을 사용합시다.") 
+        await message.delete()
+    await bot.process_commands(message)
+
+@client.event
+async def on_message(message):
+    message_content = message.content
+    bad = message_content.find("씨밸롬")
+    print(bad)
+    if bad >= 0:
+        await message.channel.send("바른말 고운말을 사용합시다.") 
+        await message.delete()
+    await bot.process_commands(message)
+
+@client.event
+async def on_message(message):
+    message_content = message.content
+    bad = message_content.find("씨뷀")
+    print(bad)
+    if bad >= 0:
+        await message.channel.send("바른말 고운말을 사용합시다.") 
+        await message.delete()
+    await bot.process_commands(message)
+
+@client.event
+async def on_message(message):
+    message_content = message.content
+    bad = message_content.find("ㄷㅊ")
+    print(bad)
+    if bad >= 0:
+        await message.channel.send("바른말 고운말을 사용합시다.") 
+        await message.delete()
+    await bot.process_commands(message)
+
+@client.event
+async def on_message(message):
+    message_content = message.content
+    bad = message_content.find("ㅆㅂ")
+    print(bad)
+    if bad >= 0:
+        await message.channel.send("바른말 고운말을 사용합시다.") 
+        await message.delete()
+    await bot.process_commands(message)
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
-
-
